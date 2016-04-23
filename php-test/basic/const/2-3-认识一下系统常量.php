734d2fd6222d5693ba505c0233fde15f2b3101b5
Poetry <jingguanliuye@gmail.com>
2016-04-23 16:44:46 +0800

    Create 2-3-认识一下系统常量.php

---
+++ php-test/basic/const/2-3-认识一下系统常量.php
@@ -0,0 +1,17 @@
+<?php
+//__FILE__ :php程序文件名。它可以帮助我们获取当前文件在服务器的物理位置。
+echo __FILE__;
+echo "<br />";
+
+//__LINE__ :PHP程序文件行数。它可以告诉我们，当前代码在第几行。
+echo __LINE__;
+echo "<br />";
+
+//PHP_VERSION:当前解析器的版本号。它可以告诉我们当前PHP解析器的版本号
+echo PHP_VERSION;
+echo "<br />";
+
+//PHP_OS：执行当前PHP版本的操作系统名称
+echo PHP_OS;
+echo "<br />";
+?>
