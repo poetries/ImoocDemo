44264ed0e97f59b6d0a8a2515a616f15d288afce
Poetry <jingguanliuye@gmail.com>
2016-04-23 13:43:17 +0800

    Create 1-4-标量类型—整型.php

---
+++ php-test/basic/var/1-4-标量类型—整型.php
@@ -0,0 +1,17 @@
+<?php
+$data_int1 = 123;
+echo $data_int1;
+echo "<br />";
+$data_int2 = -123;
+echo $data_int2;
+echo "<br />";
+$data_int3 = 0123;
+echo $data_int3;
+echo "<br />";
+$data_int4 = 0x123;
+echo $data_int4;
+echo "<br />";
+
+//整型（integer）：类似于常见的整数。它可以用十进制、八进制、十六进制指定。
+//十进制就是日常使用的数字；八进制，数字前必须加上“0”(这个0是阿拉伯数字0，可不是英文字母“欧”哦)；十六进制，数字前必须加“0x”
+?>
