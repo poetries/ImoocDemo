e0103f49eb64a16fcb4675fc2eec4f1b2c76659b
Poetry <jingguanliuye@gmail.com>
2016-04-23 22:19:29 +0800

    Create 4-14-PHP中结构嵌套之条件嵌套.php

---
+++ php-test/basic/sequential-structure/4-14-PHP中结构嵌套之条件嵌套.php
@@ -0,0 +1,26 @@
+<?php
+    $totalMoney = 0;//总工资
+	$basicMoney =  300;//基本工资
+	$sex = "男";
+	$noHouse = TRUE; //是否有房
+	$houseMoney =  150;//住房补贴
+	$isPregnancy = TRUE; //是否怀孕
+	$pregnancyMoney =  100;//怀孕补贴
+	if($sex == "男")
+	{
+		$totalMoney = $basicMoney  + 0;// 男的没奖金
+		if($noHouse)
+		{
+			$totalMoney = $totalMoney  + $houseMoney;
+		} 
+	}
+	else if($sex == "女")
+	{
+		$totalMoney = $basicMoney  + 300;// 女的有奖金300元
+	if($isPregnancy)	
+		{
+			$totalMoney = $totalMoney  + $pregnancyMoney;
+		} 
+	}
+	echo $totalMoney;
+?>
