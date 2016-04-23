5502013aa5384886943c96e94b6a7a742f0f37b4
Poetry <jingguanliuye@gmail.com>
2016-04-23 17:34:01 +0800

    Create 3-4-PHP中的比较运算符.php

---
+++ php-test/basic/Operators/3-4-PHP中的比较运算符.php
@@ -0,0 +1,28 @@
+<?php  
+    $a = 1;
+	$b = "1";
+	var_dump($a == $b);
+	echo "<br />";
+	var_dump($a === $b);
+	echo "<br />";
+	var_dump($a != $b);
+	echo "<br />";
+	var_dump($a <> $b);
+	echo "<br />";
+	var_dump($a !== $b);
+	echo "<br />";
+	var_dump($a < $b);
+	echo "<br />";
+
+	$c = 5;
+	var_dump($a < $c);
+	echo "<br />";
+	var_dump($a > $c);
+	echo "<br />";
+	var_dump($a <= $c);
+	echo "<br />";
+	var_dump($a >= $b);
+	echo "<br />";
+	var_dump($a >=$b);
+	echo "<br />";
+?>
